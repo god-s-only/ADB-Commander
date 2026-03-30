58263f27ba2b99288d20556b9e30e4baf9c68e1c
god-s-only <joshxchris098@gmail.com>
2026-03-30 21:43:04 +0100

    feat: home view model partially implemented

--- app/src/main/java/com/adbcommand/app/presentation/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/adbcommand/app/presentation/ui/features/home/HomeScreen.kt
@@ -1,9 +1,20 @@
 package com.adbcommand.app.presentation.ui.features.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.NavController
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(
+    navController: NavController,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
 
+    }
 }
