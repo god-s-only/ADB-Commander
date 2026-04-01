fe996fdb068b086425a3fa34907e5a8860d413d5
god-s-only <joshxchris098@gmail.com>
2026-04-01 18:49:58 +0100

    feat: di impl for adbkeystore manager instance and pairing repo

--- app/src/main/java/com/adbcommand/app/di/AppModule.kt
+++ app/src/main/java/com/adbcommand/app/di/AppModule.kt
@@ -1,9 +1,14 @@
 package com.adbcommand.app.di
 
+import android.content.Context
+import com.adbcommand.app.core.AdbKeyStoreManager
 import com.adbcommand.app.core.ShellCommandsExecution
+import com.adbcommand.app.data.repository.PairingRepositoryImpl
+import com.adbcommand.app.domain.repository.PairingRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,4 +20,18 @@ object AppModule {
     fun provideShellCommandsExecution(): ShellCommandsExecution {
         return ShellCommandsExecution()
     }
+
+    @Provides
+    @Singleton
+    fun provideAdbKeyStoreManager(
+        @ApplicationContext context: Context
+    ): AdbKeyStoreManager = AdbKeyStoreManager(context)
+
+
+    @Provides
+    @Singleton
+    fun providePairingRepository(
+        keyStoreManager: AdbKeyStoreManager,
+        shellExecutor: ShellCommandsExecution
+    ): PairingRepository = PairingRepositoryImpl(keyStoreManager, shellExecutor)
 }
