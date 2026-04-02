97497111ac8bd021d0cf8c343b653d0b563fe511
god-s-only <joshxchris098@gmail.com>
2026-04-02 20:52:28 +0100

    refactored home repository interface, get pairing port, get adb port, not create lol

--- app/src/main/java/com/adbcommand/app/domain/repository/HomeRepository.kt
+++ app/src/main/java/com/adbcommand/app/domain/repository/HomeRepository.kt
@@ -1,7 +1,14 @@
 package com.adbcommand.app.domain.repository
 
 interface HomeRepository {
+
     suspend fun getDeviceIp(): Result<String>
+
+    suspend fun getPairingPort(): Result<String>
+
+    suspend fun getAdbPort(): Result<String>
+
     suspend fun generatePairingCode(): Result<String>
+
     suspend fun testConnection(): Result<String>
 }
