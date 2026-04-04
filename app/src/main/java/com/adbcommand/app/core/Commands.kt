2c397059581d4a3496c7ef30fb95d6b7e01be3a2
god-s-only <joshxchris098@gmail.com>
2026-04-04 20:20:52 +0100

    feat: shell commands get device properties

--- app/src/main/java/com/adbcommand/app/core/Commands.kt
+++ app/src/main/java/com/adbcommand/app/core/Commands.kt
@@ -16,4 +16,19 @@ object Commands {
 
     fun adbConnectCommand(ip: String, adbPort: String): String =
         "adb connect $ip:$adbPort"
+
+    fun model() = "getprop ro.product.model"
+    fun manufacturer() = "getprop ro.product.manufacturer"
+    fun androidVersion() = "getprop ro.build.version.release"
+    fun apiLevel() = "getprop ro.build.version.sdk"
+    fun buildNumber() = "getprop ro.build.display.id"
+    fun securityPatch() = "getprop ro.build.version.security_patch"
+
+    fun screenSize() = "wm size"
+    fun screenDensity() = "wm density"
+    fun cpuAbi() = "getprop ro.product.cpu.abi"
+
+    fun batteryDump() = "dumpsys battery"
+
+    fun wifiDump() = "dumpsys wifi | grep 'mWifiInfo'"
 }
