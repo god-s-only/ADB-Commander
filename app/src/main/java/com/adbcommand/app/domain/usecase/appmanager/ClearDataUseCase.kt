6e2c0c10f4a066df8f308fbcdb0a0f75e89f0b55
god-s-only <joshxchris098@gmail.com>
2026-04-05 01:13:27 +0100

    feat: use case clear data use case

---
+++ app/src/main/java/com/adbcommand/app/domain/usecase/appmanager/ClearDataUseCase.kt
@@ -0,0 +1,12 @@
+package com.adbcommand.app.domain.usecase.appmanager
+
+import com.adbcommand.app.domain.models.AppActionResult
+import com.adbcommand.app.domain.repository.AppManagerRepository
+import jakarta.inject.Inject
+
+class ClearDataUseCase @Inject constructor(
+    private val repository: AppManagerRepository
+) {
+    suspend operator fun invoke(packageName: String): AppActionResult =
+        repository.clearData(packageName)
+}
