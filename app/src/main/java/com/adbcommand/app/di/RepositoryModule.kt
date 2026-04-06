1f5dabdb93707907ef2c42379384da6db4dca209
god-s-only <joshxchris098@gmail.com>
2026-04-06 20:43:11 +0100

    fix: provided instance method for shizuku app repo

--- app/src/main/java/com/adbcommand/app/di/RepositoryModule.kt
+++ app/src/main/java/com/adbcommand/app/di/RepositoryModule.kt
@@ -2,10 +2,12 @@ package com.adbcommand.app.di
 
 import android.content.Context
 import com.adbcommand.app.core.ShellCommandsExecution
+import com.adbcommand.app.data.remote.ShizukuManager
 import com.adbcommand.app.data.repository.AppManagerRepositoryImpl
 import com.adbcommand.app.data.repository.CommandsRepositoryImpl
 import com.adbcommand.app.data.repository.DeviceInfoRepositoryImpl
 import com.adbcommand.app.data.repository.HomeRepositoryImpl
+import com.adbcommand.app.data.repository.ShizukuAppManagerRepository
 import com.adbcommand.app.domain.repository.AppManagerRepository
 import com.adbcommand.app.domain.repository.CommandsRepository
 import com.adbcommand.app.domain.repository.DeviceInfoRepository
@@ -40,7 +42,7 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideAppManagerRepository(shellExecutor: ShellCommandsExecution, @ApplicationContext context: Context): AppManagerRepository {
-        return AppManagerRepositoryImpl(context, shellExecutor)
+    fun provideAppManagerRepository(shizuku: ShizukuManager, @ApplicationContext context: Context): AppManagerRepository {
+        return ShizukuAppManagerRepository(context, shizuku)
     }
 }
