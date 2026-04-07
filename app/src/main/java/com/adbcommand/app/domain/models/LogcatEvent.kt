70dd4fff818c6fb8301d8c069d39fa393d390513
god-s-only <joshxchris098@gmail.com>
2026-04-07 22:24:32 +0100

    fix: added other logcat events to logcat event event class for events lol...

--- app/src/main/java/com/adbcommand/app/domain/models/LogcatEvent.kt
+++ app/src/main/java/com/adbcommand/app/domain/models/LogcatEvent.kt
@@ -5,4 +5,12 @@ sealed class LogcatEvent {
     object Started: LogcatEvent()
     object Stopped: LogcatEvent()
     data class Error(val message: String): LogcatEvent()
+    object ToggleAutoScroll: LogcatEvent()
+    object DismissSaveResult: LogcatEvent()
+    data class LevelChanged(val level: LogLevel): LogcatEvent()
+    data class TagChanged(val tag: String): LogcatEvent()
+    data class SearchChanged(val query: String): LogcatEvent()
+    object Save: LogcatEvent()
+    object Clear: LogcatEvent()
+
 }
