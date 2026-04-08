183ce0e07739cf11335f2c75351a8e4a8ed88bcc
god-s-only <joshxchris098@gmail.com>
2026-04-08 19:33:05 +0100

    fix: id attribute to log line data class model

--- app/src/main/java/com/adbcommand/app/domain/models/LogLine.kt
+++ app/src/main/java/com/adbcommand/app/domain/models/LogLine.kt
@@ -1,6 +1,7 @@
 package com.adbcommand.app.domain.models
 
 data class LogLine(
+    val id: Long,
     val raw: String,
     val timestamp: String = "",
     val level: LogLevel = LogLevel.VERBOSE,
